13957aac9e4bb8abb69c109065b94d80806054f1
kumom <shhuang9696@gmail.com>
2018-10-22 11:47:06 +0200

    model ver1 (not using log_reg)

--- gender_and_homophily/model.py
+++ gender_and_homophily/model.py
@@ -3,13 +3,13 @@ import pandas as pd
 import itertools
 import networkx as nx
 
-PAYOFF_FUNCTION = {('tell a secret, tell a secret'): (1, 1),
-                   ('tell a secret, refrain'): (-1, 0),
-                   ('refrain, tell a secret'): (0, -1),
-                   ('refrain, refrain'): (0, 0)}
+PAYOFF_FUNCTION = {('tell a secret', 'tell a secret'): (1, 1),
+                   ('tell a secret', 'refrain'): (-1, 0),
+                   ('refrain', 'tell a secret'): (0, -1),
+                   ('refrain', 'refrain'): (0, 0)}
 
 class StudentAgent():
-    def __init__(self, model, id, gender, trust_agents, affective):
+    def __init__(self, model, idx, gender, trust_agents, affective):
         """
         Create a new agent
 
@@ -23,10 +23,10 @@ class StudentAgent():
         """
 
         self.model = model
-        self.id = id
+        self.id = idx
         self.gender = gender
         self.trust_agents = trust_agents
-        self.prob_trust = len(trust_agents) / len(self.model.agents)
+        self.prob_trust = len(trust_agents) / len(affective)
         self.affective = affective
         self.strategy = None
 
@@ -36,15 +36,17 @@ class StudentAgent():
         else:
             bonus_for_gender = self.model.bonus_m2m if self.gender == opponent.gender else 0
 
-        bonus_for_friends = self.model.bonus_for_friends if self.affective[opponent.id] == 2 else 0
+        bonus_for_friends = self.model.bonus_for_friends if self.affective[str(opponent.id)] == 2 else 0
 
         prob_cooperate = self.prob_trust + bonus_for_gender + bonus_for_friends \
                          if opponent not in self.trust_agents else 1
+        if prob_cooperate > 1:
+            prob_cooperate = 1
 
-        return np.random.choice(['tell a secret', 'refrain'], [prob_cooperate, 1 - prob_cooperate])
+        return np.random.choice(a = ['tell a secret', 'refrain'], p = [prob_cooperate, 1 - prob_cooperate])
 
     def state_update(self):
-        self.prob_trust = len(self.trust_agents) / len(self.model.players)
+        self.prob_trust = len(self.trust_agents) / len(self.affective)
 
 
 
@@ -65,14 +67,15 @@ class TrustModel():
         self.bonus_m2m = bonus_m2m
         self.bonus_f2f = bonus_f2f
         self.bonus_for_friends = bonus_for_friends
-        self.players = []
+        self.agents = []
 
-        for id, attributes in self.trust_network.nodes(data = True):
-            trust_agents = [n for n in trust_network.neighbors[id]]
+        for idx, attributes in self.trust_network.nodes(data = True):
+            trust_agents = [n for n in trust_network.neighbors(idx)]
+            # Initialize the affective scores from one agent (= id) to all other agents
             affective = {}
-            for column_idx, value in affective_matrix[id]:
-                affective[column_idx] = value
-            self.players.append(StudentAgent(self, id, attributes['sex'], set(trust_agents), affective))
+            for column_idx in affective_matrix.loc[[idx]]:
+                affective[column_idx] = affective_matrix.loc[[idx]][column_idx].values[0]
+            self.agents.append(StudentAgent(self, idx, attributes['sex'], set(trust_agents), affective))
 
     def step(self):
         """
@@ -80,47 +83,47 @@ class TrustModel():
         """
 
         ## Update affective_matrix according the payoff function
-        for a, b in itertools.combinations(self.players, 2):
-            strategy = (a.play(a, b), b.play(b, a))
+        for a, b in itertools.combinations(self.agents, 2):
+            strategy = (a.play(b), b.play(a))
             a_payoff, b_payoff = PAYOFF_FUNCTION[strategy]
-            a.affective[b] += a_payoff
-            b.affective[a] += b_payoff
+            a.affective[str(b.id)] += a_payoff
+            b.affective[str(a.id)] += b_payoff
 
-            if a.affective[b] > 2:
-                a.affective[b] = 2
-            if a.affective[b] < -2:
-                a.affective[b] = -2
+            if a.affective[str(b.id)] > 2:
+                a.affective[str(b.id)] = 2 
+            if a.affective[str(b.id)] < -2:
+                a.affective[str(b.id)] = -2
 
-            if b.affective[a] > 2:
-                b.affective[a] = 2
-            if b.affective[a] < -2:
-                b.affective[a] = -2
+            if b.affective[str(a.id)] > 2:
+                b.affective[str(a.id)] = 2 
+            if b.affective[str(a.id)] < -2:
+                b.affective[str(a.id)] = -2 
 
-            self.affective_matrix[a][b] = a.affective[b]
-            self.affective_matrix[b][a] = b.affective[a]  
+            self.affective_matrix[str(a.id)][str(b.id)] = a.affective[str(b.id)]
+            self.affective_matrix[str(b.id)][str(a.id)] = b.affective[str(a.id)]  
 
             ## Rewiring in the trust network
             # Case 1
             if strategy == ('tell a secret', 'tell a secret'):
-                if (a.affective[b.id] == 2) and (b not in a.trust_agents):
+                if (a.affective[str(b.id)] == 2) and (b not in a.trust_agents):
                     # If to_trust = 1, we create a new edge from a to b, otherweise unchanged
-                    to_trust = np.random.choice([0, 1], [1 - self.bonus_for_friends, self.bonus_for_friends])
+                    to_trust = np.random.choice(a = [0, 1], p = [1 - self.bonus_for_friends, self.bonus_for_friends])
                     if to_trust == 1:
                         a.trust_agents.add(b.id)
-                        self.trust_network.add(a.id, b.id)
+                        self.trust_network.add_edge(a.id, b.id)
 
-                if (b.affective[a.id] == 2) and (a not in b.trust_agents):
+                if (b.affective[str(a.id)] == 2) and (a not in b.trust_agents):
                     # If to_trust = 1, we create a new edge from b to a, otherweise unchanged
-                    to_trust = np.random.choice([0, 1], [1 - self.bonus_for_friends, self.bonus_for_friends])
+                    to_trust = np.random.choice(a = [0, 1], p = [1 - self.bonus_for_friends, self.bonus_for_friends])
                     if to_trust == 1:
                         b.trust_agents.add(a.id)
-                        self.trust_network.add(b.id, a.id)
+                        self.trust_network.add_edge(b.id, a.id)
             # Case 3
             if strategy == ('refrain', 'tell a secret') and self.trust_network.has_edge(b.id, a.id):
-                self.trust_network.remove(b.id, a.id)
+                self.trust_network.remove_edge(b.id, a.id)
             # Case 4
             if strategy == ('tell a secret', 'refrain') and self.trust_network.has_edge(a.id, b.id):
-                self.trust_network.remove(a.id, b.id)
+                self.trust_network.remove_edge(a.id, b.id)
 
             ## Update prob_trust of both agents
             a.state_update()
--- gender_and_homophily/run.py
+++ gender_and_homophily/run.py
@@ -1,9 +1,8 @@
 from model import *
 
-def run(num_step, trust_network, affective_matrix, corr_friend_trust, bonus_m2m, bonus_f2f, bonus_for_friends):
+def run(num_step, trust_network, affective_matrix, bonus_m2m, bonus_f2f, bonus_for_friends):
     model = TrustModel(trust_network = trust_network, affective_matrix = affective_matrix, \
-                       corr_friend_trust = corr_friend_trust, bonus_m2m = bonus_m2m, \
-                       bonus_f2f = bonus_f2f, bonus_for_friends = bonus_for_friends)
+                       bonus_m2m = bonus_m2m, bonus_f2f = bonus_f2f, bonus_for_friends = bonus_for_friends)
 
     networks = []
     current_step = 0
